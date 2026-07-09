becde5026f77fd071cf4663f82e76f8ad082c774
ReginaldUG <78037636+ReginaldUG@users.noreply.github.com>
2026-07-09 09:15:35 +0100

    Assembly and migration changes

--- BusBooking.Core/Constants/AccountStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace BusBooking.Core.Constants;
-
-public static class AccountStatus
-{
-    public const string Active = "active";
-    public const string NotActive = "notactive";
-    public const string Suspended = "suspended";
-    public const string Deleted = "deleted";
-}
--- BusBooking.Models/obj/Debug/net10.0/BusBooking.Models.AssemblyInfo.cs
+++ BusBooking.Models/obj/Debug/net10.0/BusBooking.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusBooking.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cab12223731a94eef146d6890a45b257d5f438fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01546163ea08c62aa26b9376eeb1ca5ddd43958f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusBooking.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusBooking.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
